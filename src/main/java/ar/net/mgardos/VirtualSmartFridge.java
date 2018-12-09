e887497f393d45d84e8fce053aa837b57e705a6c
mgardos <mariano.gardos@gmail.com>
2018-12-09 16:39:23 -0300

    Initial commit: Scenario of fridge turned on with no products inside (Inside-out TDD approach)

--- src/main/java/ar/net/mgardos/VirtualSmartFridge.java
+++ src/main/java/ar/net/mgardos/VirtualSmartFridge.java
@@ -1,3 +1,24 @@
+/*
+   This file is part of the Virtual Smart Fridge.
+
+   Virtual Smart Fridge is a software that emulates the basic functionality
+   of a truly smart fridge.
+   Copyright (C) 2018 Mariano E. Gardos Carro
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   The Virtual Smart Fridge is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package ar.net.mgardos;
 
 import java.util.ArrayList;
